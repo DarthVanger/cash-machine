3c23e12082b811b1e747a4cce50b94c69f6a1fa1
DarthVanger <darthvanger@gmail.com>
2015-08-22 14:08:07 +0300

    Started developing login-view

--- Angular/app/login-view/login-view.js
+++ Angular/app/login-view/login-view.js
@@ -1,10 +1,10 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.loginView', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
+  $routeProvider.when('/login-view', {
+    templateUrl: 'login-view/login-view.html',
     controller: 'View1Ctrl'
   });
 }])
