47404c7680899220641f897bb702e87c7ce6c8ae
DarthVanger <darthvanger@gmail.com>
2015-08-22 19:15:50 +0300

    Developed login form submitting (without ajax request yet)

--- Angular/app/components/pin-code-keyboard/pin-code-keyboard-directive.js
+++ Angular/app/components/pin-code-keyboard/pin-code-keyboard-directive.js
@@ -5,6 +5,12 @@ angular.module('cashMachine.pinCodeKeyboard.pinCodeKeyboard-directive', [])
 .directive('pinCodeKeyboard', [function() {
     return {
         templateUrl: 'components/pin-code-keyboard/pin-code-keyboard.html',
+
+        scope: {
+            pinCode: '=',
+            onSubmit: '&'
+        },
+
         link: function(scope, element, attrs) {
             scope.pinCode = '';
             console.log('element:');
@@ -19,7 +25,11 @@ angular.module('cashMachine.pinCodeKeyboard.pinCodeKeyboard-directive', [])
 
             scope.deleteCharacter = function() {
                 scope.pinCode = scope.pinCode.substring(0, scope.pinCode.length - 1);
-            }
+            };
+
+            scope.submitPinCode = function() {
+                scope.onSubmit({pinCode: scope.pinCode});
+            };
         }
     };
 }]);
--- Angular/app/login-view/login-view.js
+++ Angular/app/login-view/login-view.js
@@ -10,12 +10,36 @@ angular.module('cashMachine.loginView', ['ngRoute'])
 }])
 
 .controller('LoginViewCtrl', ['$scope', function($scope) {
+
+    $scope.cardholderInfo = {
+        cardNumber: '',
+        pinCode: '' 
+    };
+    
     console.log('login view!');
     $scope.showVirtualKeyboard = function() {
-        console.log('keyboard! :)');
+        $('.pin-code-keyboard-modal').show();
     };
-    //$scope.cardholderInfo = {
-    //    cardNumber: 12,
-    //    pinCode: 24
-    //};
+
+    $scope.hideVirtualKeyboard = function() {
+        $('.pin-code-keyboard-modal').hide();
+    };
+
+    /**
+     * Is called from pin-code-keyboard directive
+     */
+    $scope.pinCodeWasEntered = function(pinCode) {
+        $scope.hideVirtualKeyboard();
+        $scope.cardholderInfo.pinCode = pinCode;
+    };
+
+    $scope.submitLogin = function() {
+        if ($scope.cardholderLoginForm.$valid) {
+            console.log('submittting! :)');
+            console.log('cardholder info:');
+            console.log($scope.cardholderInfo);
+        } else {
+            console.log('form invalid! :)');
+        }
+    };
 }]);
